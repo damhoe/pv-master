a9464cdb5f350cb36d093c9ab5d33dd9c29077ac
damhoe <damian.hoedtke@gmx.de>
2021-11-15 15:19:27 +0100

    history-sparse data

--- submit.sh
+++ submit.sh
@@ -5,9 +5,10 @@
 #SBATCH --mem=20gb
 #SBATCH --job-name=hoedtke_sim_pv_e
 #SBATCH --account=compacts
-#SBATCH --output=data/sim1000k/history-sparse/pop.log
-#SBATCH --error=data/sim1000k/history-sparse/pop.err
+#SBATCH --output=sim_%j_seed_1.log
+#SBATCH --error=sim_%j_seed_1.err
 #SBATCH --workdir=/home/damianho/projects/git/PV_master/
 
 module load anaconda/2020.07
-python3 src/pop_model_efficient.py
+python3 src/pop_model_efficient.py 1000 1overr2
+mv sim_${SLURM_JOB_ID}_seed_1.* data/sim1000k/history-sparse/1overr2/
