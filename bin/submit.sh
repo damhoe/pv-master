1fb77abc8991c57e9219fcf1abb92d7220207fe3
damhoe <damian.hoedtke@gmx.de>
2021-11-23 13:37:54 +0100

    cleaned folders

--- bin/submit.sh
+++ bin/submit.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 #SBATCH --qos=short
-#SBATCH --time=300
-#SBATCH --mem=20gb
-#SBATCH --job-name=hoedtke_sim_pv_e
+#SBATCH --time=1-00:00:00
+#SBATCH --mem=30gb
+#SBATCH --job-name=hoedtke_sim_pv
 #SBATCH --account=compacts
-#SBATCH --output=sim_%j_seed_1.log
-#SBATCH --error=sim_%j_seed_1.err
-#SBATCH --workdir=/home/damianho/projects/git/PV_master/
+#SBATCH --output=sim_%j.log
+#SBATCH --error=sim_%j.err
+#SBATCH --workdir=/home/damianho/projects/git/pv-master/
 
 module load anaconda/2020.07
 python3 src/pop_model_efficient.py 1000 1overr2
-mv sim_${SLURM_JOB_ID}_seed_1.* data/sim1000k/history-sparse/1overr2/
+mv sim_${SLURM_JOB_ID}.* data/sim1000k/history-sparse/1overr2/
